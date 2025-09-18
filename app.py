22f9b51082a0ae0d3b628f8a3a4ebbb55661a254
Tatiana Brimm <tbrimm1@student.gsu.edu>
2025-09-17 23:42:30 -0400

    Update app.py

--- app.py
+++ app.py
@@ -72,7 +72,7 @@ app.add_middleware(
     allow_origins=[
         "http://127.0.0.1:5500",
         "http://localhost:5500",
-        "https://lead-gen-ai-frontend-omega.vercel.app/"
+        "https://lead-gen-ai-frontend-omega.vercel.app"
     ],  # frontend URL
     allow_credentials=True,
     allow_methods=["*"],
